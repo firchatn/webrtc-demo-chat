332a1fc74186bcbd00d4b93bedc592b4cea2a6ad
Firas Chaaben <firaschaabencss@gmail.com>
2018-02-17 23:21:06 +0100

    testing 2 client fail

--- stream.js
+++ stream.js
@@ -1,12 +1,13 @@
-'use strict';
+  var id2;
+  var peer1 = new Peer({key: 'l15v4rrvzkcvj9k9'});
+  var peer2 = new Peer({key: 'r64168xoht4yrpb9'});
+  var stream;
+  var x = document.getElementById("myAudio");
 
 
-// Put variables in global scope to make them available to the browser console.
-var audio = document.querySelector('audio');
 
 var clickerFn;
 var el = document.getElementById('p');
-var x = document.getElementById("myAudio");
     clickerFn = function() {
     console.log('Click just happened');
     if (x.paused) {
@@ -19,7 +20,20 @@ var x = document.getElementById("myAudio");
 }
 
 el.addEventListener('click', clickerFn);
-alert("end");
+
+
+peer1.on('open', function(id) {
+  console.log('I\'m peer1 and my id is: ' + id);
+});
+
+
+peer2.on('open', function(id2) {
+  console.log('I\'m peer2 and my id is: ' + id2);
+});
+
+
+
+var audio = document.querySelector('audio');
 
 var constraints = window.constraints = {
   audio: true,
@@ -44,3 +58,19 @@ function handleError(error) {
 navigator.mediaDevices.getUserMedia(constraints).
     then(handleSuccess).catch(handleError);
 
+
+mediaStream = stream;
+
+
+peer1.call(id2, mediaStream);
+// answer call from peer1
+peer2.on('call', function(call) {
+  call.answer(mediaStream);
+});
+
+call.on('stream', function(stream) {
+  // `stream` is the MediaStream of the remote peer.
+  // Here you'd add it to an HTML video/canvas element.
+  audio.srcObject = stream;
+
+});

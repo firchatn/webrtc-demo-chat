ca00fb78486567f38fb31cdf9ff8d9420030148e
Firas Chaaben <firaschaabencss@gmail.com>
2018-02-18 12:26:42 +0100

    fix struct v Demo

--- js/login.js
+++ js/login.js
@@ -1,8 +1,11 @@
+
 function login(){
     var email = document.getElementById("idemail").value;
     var pass = document.getElementById("idpass").value;
     if (email == 'firas@tik.tn' && pass == '1234'){
       window.open ('file:///home/firas/git/talkiwalki/talkiwalki.html','_self',false);
+      alert("your welcome");
+      
     }
 
 

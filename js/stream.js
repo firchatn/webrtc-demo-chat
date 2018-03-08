f37ec06ba39f29c36c77c5f524d8d148b9c553c3
Firas Chaaben <firaschaabencss@gmail.com>
2018-03-08 10:10:49 +0100

    update data

--- js/stream.js
+++ js/stream.js
@@ -4,7 +4,7 @@ var peer = new Peer(1,{key: 'l15v4rrvzkcvj9k9'});
 function calll(){
 navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 navigator.getUserMedia({video: false, audio: true}, function(stream) {
-  var call = peer.call('2', stream);
+  var call = peer.call(2, stream);
   call.on('stream', function(remoteStream) {
     // Show stream in some <video> element.
 console.log('stream on');
